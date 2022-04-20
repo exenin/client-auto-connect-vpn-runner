0edba45222bc3c0c7ccfb6dfcd45d3426a55b77a
Your Name <jean@tryb.co.za>
2022-04-20 14:04:50 -0400

    append

--- scripts/install-this-repo.sh
+++ scripts/install-this-repo.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/exenin/client-auto-connect-vpn-runner.git /opt/vpn
-
+git clone https://github.com/exenin/client-auto-connect-vpn-runner.git /opt/vpn || echo exists
 cd /opt/vpn
+git pull
 
 echo "Installing tools - tmux + openvpn"
 bash vpn/install.sh
