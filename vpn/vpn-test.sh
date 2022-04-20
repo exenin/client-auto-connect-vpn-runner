c5349004bb267e60a8ca217d9a87570e8c4bb0ed
Your Name <jean@tryb.co.za>
2022-04-20 14:13:06 -0400

    append

--- vpn/vpn-test.sh
+++ vpn/vpn-test.sh
@@ -1,5 +1,6 @@
 is_running=$(ps -axu | grep --silent -e vpn -e root -e  c.ovpn 2>/dev/null && echo True || echo False)
 wd=/opt/vpn/vpn
+source /opt/vpn/VPN_EXPECTED_ID
 cd $wd
 echo 'vpn is running?', $is_running
 
